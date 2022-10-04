d3aad8cfcaf4d33b7c6a88aea4ab9eba0ba205c1
Quincy Jones <rqjones@asu.edu>
2022-10-04 13:31:25 -0700

    counter added

--- contracts/lemodadeStand.sol
+++ contracts/lemodadeStand.sol
@@ -1,9 +1,10 @@
 contract lemonadeStand is XRCSHARE{
+    uint public lemonadeCount =0;
     constructor(string  memory _name,string  memory _symbol,uint _totalSupply) XRCSHARE(_name,_symbol,_totalSupply){}
-
+    
     function GlassOfLemonade()  public  payable  returns(bool){
         require(msg.value >=  10000000000000000000,"need more funds");
-
+        lemonadeCount++;
         //this call can be used in all functions that recive fees
         redirectValue(msg.value);
         return  true;
