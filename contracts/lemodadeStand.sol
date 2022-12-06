0f1a53a930f9105aa49f0b36c53a38de28622c8f
Quincy Jones <rqjones@asu.edu>
2022-12-06 13:20:17 -0700

    lemonade stand License  and dependancies

--- contracts/lemodadeStand.sol
+++ contracts/lemodadeStand.sol
@@ -1,6 +1,11 @@
+pragma solidity ^0.8.10;
+// SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./SHARE.sol";
+
 contract lemonadeStand is XRCSHARE{
     uint public lemonadeCount =0;
-    constructor(string  memory _name,string  memory _symbol,uint _totalSupply) XRCSHARE(_name,_symbol,_totalSupply){}
+    constructor(string  memory _name,string  memory _symbol,uint _totalSupply,string memory _URI) XRCSHARE(_name,_symbol,_totalSupply,_URI){}
     
     function GlassOfLemonade()  public  payable  returns(bool){
         require(msg.value >=  10000000000000000000,"need more funds");
